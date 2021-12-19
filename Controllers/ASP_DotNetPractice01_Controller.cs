6bcc32dffce65c39ff024d22e4bc9ad92a37d3a2
Abhishek Dubey <abhishekd905@gmail.com>
2021-12-19 19:20:49 +0530

    Add project files.

---
+++ Controllers/ASP_DotNetPractice01_Controller.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet_MVC_Practice_01.Controllers
+{
+    public class ASP_DotNetPractice01_Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string list()
+        {
+            return "List function";
+        }
+
+        [ActionName("CurrentTime")]
+        public string GetCurrentTime()
+        {
+            return DateTime.Now.ToString("T");
+        }
+    }
+}
---
+++ Models/StudentDataModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet_MVC_Practice_01.Models
+{
+    public class StudentDataModel
+    {
+    }
+}
